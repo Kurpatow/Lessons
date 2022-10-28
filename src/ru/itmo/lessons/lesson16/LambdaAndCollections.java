2f736156db3be4888e4f8f43fffbb76a25048e5b
Nikita Kurpatow <kurpatowph@yandex.ru>
2022-10-28 15:52:24 +0300

    add lessons info

--- src/ru/itmo/lessons/lesson16/LambdaAndCollections.java
+++ src/ru/itmo/lessons/lesson16/LambdaAndCollections.java
@@ -3,10 +3,53 @@ package ru.itmo.lessons.lesson16;
 import ru.itmo.lessons.lesson16.education.Course;
 import ru.itmo.lessons.lesson16.education.University;
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
 public class LambdaAndCollections {
     public static void main(String[] args) {
         University university = new University();
         university.addCourses(Course.getInstances(15));
+
+        //перебор коллекции - метод foreach
+        // Consumer<T> - void accept (T , t);
+        Consumer<Course> soutCourse = course -> System.out.println(course);
+        university.getCourses().forEach(soutCourse);
+        university.getCourses().forEach(course -> System.out.println(course.getName()));
+
+        //увеличить стоимость круса на 10%
+        university.getCourses().forEach(course -> course.setPrice(course.getPrice()));
+
+        //вывести название курсов продолжительностью меньше 3 мес
+        Consumer<Course> courses = course -> {
+            if (course.getDuration() < 3) {
+                System.out.println(course.getName());
+            }
+        };
+        university.getCourses().forEach(courses);
+
+        // Predicate<T> boolean test (T, t);
+        // true, если курс дешевле 20_000
+        // true, если продолжительность курса 3 месяца и меньше
+        //true, если название курса jjd без учета  регистра
+
+        Predicate<Course> byPrice = course -> course.getPrice() < 20_000;
+        Predicate<Course> byDuration = course -> course.getDuration() <= 3;
+        Predicate<Course> byName = course -> course.getName().equalsIgnoreCase("jjd");
+
+        university.getFilteredCourses(byPrice).forEach(course -> System.out.println(course));
+
+
+        // удаление методом removeIf
+        //Predicate<T> boolean test(T, t);
+        university.getCourses().removeIf(course -> course.getPrice() < 15_000);
+        university.getCourses().forEach(course -> System.out.println(course));
+
+        //сортировка (Comparator)
+        // Comparator - int compare(T o1, T o2);
+        Comparator<Course> compareByName = (course01, course02) -> course01.getName().compareTo(course02.getName());
 
     }
 }
--- src/ru/itmo/lessons/lesson16/LambdaLesson.java
+++ src/ru/itmo/lessons/lesson16/LambdaLesson.java
@@ -1,7 +1,14 @@
 package ru.itmo.lessons.lesson16;
 
+import javax.swing.*;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 public class LambdaLesson {
+    private static void calculate(Operation operation, double x, double y) {
+        System.out.println("Результат: " + operation.execute(x, y));
+    }
+
     public static void main(String[] args) {
         // объявить интерфейс Operation
         // с абстрактным методом execute,
@@ -16,9 +23,29 @@ public class LambdaLesson {
         // если метод принимает на вход один аргумент, то круглые скобки можно опустить (на свое усмотрение)
         Operation plus = (first, second) -> first + second;
         Operation div = (a, b) -> {
-          if (b == 0) return 0;
-          return a / b;
+            if (b == 0) return 0;
+            return a / b;
         };
+        Predicate<Integer> isPos = x -> x > 0;
+        Predicate<Integer> isNeg = x -> x < 0;
+        Predicate<Integer> isEven = x -> x % 2 == 0;
+
+        System.out.println(isPos.test(-90));
+        System.out.println(isPos.and(isEven).test(100));
+        System.out.println(isNeg.or(isPos).test(0));
+
+        //
+        //
+        //
+        //
+        Function<Integer, Double> minus20 = x -> x * 0.8;
+        Function<Integer, Integer> doubled = x -> x * 2;
+        Function<Integer, String> str = x -> {
+          if (x <= 0) throw new IllegalArgumentException("Число д.б положительным");
+                return x + "р.";
+        };
+        System.out.println(doubled.apply(100));
+        System.out.println(doubled.andThen(str).apply(12));
     }
 }
 
